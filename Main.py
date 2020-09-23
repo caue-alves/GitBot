69f7ff6a4ed568381783a1bf5aa9302e6c3abc16
D4rk-pixel <caue.mendes.rodrigues.alves@gmail.com>
2020-09-23 17:34:25 -0300

    GitBot Commit

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import json
 from os import system
 from rich import print
 
-input("[bold magenta]Bem vindo ao GitBot!, digite enter para começar[/bold magenta]")
+print("[bold magenta]Bem vindo ao GitBot!, digite enter para começar[/bold magenta]", locals())
 with open("infra/gbconfig.json", "r") as jj:
     f = json.load(jj)
     system(f'cd {f["path"]}')
